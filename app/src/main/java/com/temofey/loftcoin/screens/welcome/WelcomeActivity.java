0272a94dccb3a4e1e1aedfd8e844f112de93fa0b
Artemy <temofey_k@mail.ru>
2018-10-08 00:05:10 +0300

    added tab dots

--- app/src/main/java/com/temofey/loftcoin/screens/welcome/WelcomeActivity.java
+++ app/src/main/java/com/temofey/loftcoin/screens/welcome/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,9 @@ public class WelcomeActivity extends AppCompatActivity {
     @BindView(R.id.start_btn)
     Button startBtn;
 
+    @BindView(R.id.tabDots)
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,8 @@ public class WelcomeActivity extends AppCompatActivity {
         final Prefs prefs = ((App) getApplication()).getPrefs();
 
         pager.setAdapter(new WelcomePagerAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(pager, true);
+
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
