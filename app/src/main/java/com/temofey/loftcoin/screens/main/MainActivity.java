dd6599d692932cb445c2d879ec2ff90675aa6b76
Artemy <temofey_k@mail.ru>
2018-10-17 19:05:47 +0300

    added rate fragment

--- app/src/main/java/com/temofey/loftcoin/screens/main/MainActivity.java
+++ app/src/main/java/com/temofey/loftcoin/screens/main/MainActivity.java
@@ -4,8 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.temofey.loftcoin.R;
+import com.temofey.loftcoin.screens.main.rate.RateFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +24,52 @@ public class MainActivity extends AppCompatActivity {
         context.startActivity(starter);
     }
 
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        navigation.setOnNavigationItemSelectedListener(navigationListener);
+        navigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+
+            }
+        });
+
+        if (savedInstanceState == null) {
+            navigation.setSelectedItemId(R.id.menu_item_rate);
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.menu_item_accounts:
+                    break;
+
+                case R.id.menu_item_rate:
+                    showRateFragment();
+                    break;
+
+                case R.id.menu_item_converter:
+                    break;
+            }
+
+            return true;
+        }
+    };
+
+    private void showRateFragment() {
+        RateFragment fragment = new RateFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
